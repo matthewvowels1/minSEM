ad884f4610b2c9c9f25f44d5f93fc0e98d7a1b56
Matthew Vowels <matthewvowels1@btinternet.com>
2022-04-20 11:09:20 +0200

    Increase running time and fixed remaining bugs. Added precision argument to retain or remove precision / risk vars

--- reducer.py
+++ reducer.py
@@ -1,86 +1,85 @@
 import networkx as nx
 
-def reducer(graph, xs, ys):
 
-	reduced_graph = nx.DiGraph()
+def reducer(graph, xs, ys, remove_precision=True):
+	reduced_graph = graph.copy()
+	reduced_graph.remove_nodes_from(list(nx.isolates(reduced_graph)))
 
-	desired_paths = []
-	for x, y in zip(xs, ys):
-		desired_paths.append(list(nx.all_simple_paths(graph, x, y)))
+	# remove nodes <after> ys:
+	ordering = list(nx.topological_sort(reduced_graph))
+	indexes = []
+	for var in ys:
+		indexes.append(ordering.index(var))
 
-	desired_vars = []
-	for desired_path in desired_paths:
-		for path in desired_path:
-			edges = len(path)
-			desired_vars.append(v for v in path)
-			for i in range(1, edges):
-				reduced_graph.add_edge(path[i - 1], path[i])
+	max_index = max(indexes)
+	remove_vars = ordering[max_index + 1:]
 
-	desired_paths = list(reduced_graph.edges())
-	desired_vars = set([item for sublist in desired_paths for item in sublist])
+	for var in remove_vars:
+		reduced_graph.remove_node(var)
 
-	all_predecs = []
-	for desired_var in desired_vars:  # for each desired variable
-		predecs = list(graph.predecessors(desired_var))  # get desired var predecessors
-		all_predecs.append(
-			[p for p in predecs if p not in desired_vars])  # remove the predecessors which are desired variable
-	all_predecs = set([item for sublist in all_predecs for item in sublist])
+	# init confounder dictionary (based on causal paths from root causes)
+	confounders = {}
+	# init causally relevant variable dictionary
+	all_causally_relevant_vars = []
+	all_causally_relevant_vars.extend(xs)
+	# init causal chain dictionary - this will keep track of children based on root causes
+	causal_chain = {}
+	for x in xs:
+		causal_chain[x] = [x]
+		effect_vars = list(nx.descendants(reduced_graph, x))
+		# add causally relevant vars to dict of causally relevant vars
+		all_causally_relevant_vars.extend(effect_vars)
+		all_causally_relevant_vars = list(set(all_causally_relevant_vars))
 
-	routes = {}  # this will store all the ways a predec can connect to a desired variable
-	for predec in all_predecs:
-		routes[predec] = []
-		for desired_var in desired_vars:
-			paths = list(nx.all_simple_paths(graph, predec, desired_var))
+	# get list of all nodes
+	all_nodes = set(list(reduced_graph.nodes))
+	# identify list of non-causal nodes
+	non_causal_nodes = all_nodes - set(all_causally_relevant_vars)
 
-			for path in paths:
-				for var in path:
-					if var in desired_vars:  # for each variable in the path, if we find a desired variable we store it and move on
-						routes[predec].append(var)
-						break
+	for k in range(2):  # while length > 0:
 
-	# turns the routes into sets to avoid duplication
-	for key in routes:
-		routes[key] = set(routes[key])
+		for x in xs:
+			current_cause = causal_chain[x][k]
+			if k < 1:
+				confounders[x] = []
 
-	# any vars with only one route to desired variables is either instrumental or precision var
-	to_remove = []
-	for key in routes:
-		if len(routes[key]) <= 1:
-			to_remove.append(key)
-	for key in to_remove:
-		routes = {k: v for k, v in routes.items() if k != key}
+			conf_vars_ = list(nx.ancestors(reduced_graph, current_cause))
+			conf_vars = []
+			[conf_vars.append(v) for v in conf_vars_ if v not in all_causally_relevant_vars]
 
-	# use the routes dict to add all confounding edges
-	for key in routes:
-		route = routes[key]
-		for var in route:
-			reduced_graph.add_edge(key, var)
+			for potential_confounder in conf_vars:
+				pot_conf_descs = list(nx.descendants(reduced_graph, potential_confounder))
 
-	# now we can remove some of these edges if they are the first along a path, first we identify these positions
-	ordering = {}
-	for key in routes:
-		route = routes[key]
-		ordering[key] = {}
-		for var in route:
-			for var2 in route:  # count the maximum position of each of the desired vars in each confounded route
-				paths = list(nx.all_simple_paths(reduced_graph, var, var2))
-				if len(paths) != 0:
-					for path in paths:
-						for var3 in desired_vars:
-							try:
-								index = path.index(var3)
-								ordering[key][var3] = index
-							except:
-								pass
+				if any(p in pot_conf_descs for p in effect_vars):
+					if (potential_confounder not in confounders[x]) and (
+							potential_confounder not in all_causally_relevant_vars):
+						reduced_graph.remove_edge(potential_confounder, current_cause)
+						confounders[x].append(potential_confounder)
 
-	# we now remove the edges to the nodes with zeroth positions in the causal ordering (if they exist at all)
-	for key in ordering:
-		position_dict = ordering[key]
-		for key2 in position_dict:
-			if position_dict[key2] == 0:
-				try:
-					reduced_graph.remove_edge(key, key2)
-				except:
-					pass
+			children = list(reduced_graph.successors(current_cause))
+			causal_chain[x].extend(children)
 
+		# compile list of all identified confounders
+	all_confounders = []
+	for key in confounders:
+		all_confounders.append(confounders[key])
+	all_confounders = set([item for sublist in all_confounders for item in sublist])
+
+	# find remaining nodes which are neither causal nor confounders (e.g. precisions and colliders)
+	remaining_nodes = non_causal_nodes - all_confounders
+
+	if remove_precision:  # remove precision vars
+		for remaining_node in remaining_nodes:
+			# check ancestors and descendents
+			remaining_decs = list(nx.descendants(reduced_graph, remaining_node))
+			remaining_ancs = list(nx.ancestors(reduced_graph, remaining_node))
+			if any(x in remaining_ancs for x in all_confounders) == False:
+				reduced_graph.remove_node(remaining_node)
+
+	# finally clean up graph by removing isolated vars
+	reduced_graph.remove_nodes_from(list(nx.isolates(reduced_graph)))
 	return reduced_graph
+
+
+
+
